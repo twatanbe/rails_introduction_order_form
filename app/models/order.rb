ffe75e6a0b670db85bf8097073d3eb184084749a
twatanabe <t.watanabe.yami@gmail.com>
2025-09-20 11:24:44 +0900

    バリデーションパターンにメールアドレスを入力した際の正規表現によるチェックを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,13 @@ class Order < ApplicationRecord
   validates :email, presence: true, length: { maximum:100 }
   validates :telephone, presence: true, length: { maximum:11 }, numericality: { only_integer: true }
   validates :delivery_address, presence: true, length: { maximum:100 }
+  validate :email_format
+
+  private
+
+  def email_format
+    return if /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match?(email)
+
+    errors.add(:email, 'の形式が正しくありません')
+  end
 end

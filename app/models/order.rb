257cc30bc74fc57fd8069436697a48a6d0cba66d
twatanabe <t.watanabe.yami@gmail.com>
2025-09-25 17:55:22 +0900

    コールバックで入力内容を変換する処理を追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,23 @@
+require 'nkf'
+
 class Order < ApplicationRecord
   validates :name, presence: true, length: { maximum:40 }
   validates :email, presence: true, length: { maximum:100 }, email_format: true
   validates :telephone, presence: true, length: { maximum:11 }, numericality: { only_integer: true }
   validates :delivery_address, presence: true, length: { maximum:100 }
+
+  after_initialize :format_telephone
+  after_initialize :format_email
+
+  private
+
+  def format_telephone
+    return if telephone.blank?
+    self.telephone = telephone.tr('０-９', '0-9').delete('^0-9')
+  end
+
+  def format_email
+    return if email.blank?
+    self.email = NKF.nkf('-w -Z4', email)
+  end
 end

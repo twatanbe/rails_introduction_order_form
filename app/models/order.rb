aafe3a9bf4a5878835fd773a0d500f3266864f2d
twatanabe <t.watanabe.yami@gmail.com>
2025-09-25 16:07:39 +0900

    バリデーション専用のクラスの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,6 @@
 class Order < ApplicationRecord
   validates :name, presence: true, length: { maximum:40 }
-  validates :email, presence: true, length: { maximum:100 }
+  validates :email, presence: true, length: { maximum:100 }, email_format: true
   validates :telephone, presence: true, length: { maximum:11 }, numericality: { only_integer: true }
   validates :delivery_address, presence: true, length: { maximum:100 }
-  validate :email_format
-
-  private
-
-  def email_format
-    return if /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match?(email)
-
-    errors.add(:email, 'の形式が正しくありません')
-  end
 end

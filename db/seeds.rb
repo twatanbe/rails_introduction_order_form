5e7c31543169d460a4be30a9a87c7d9427af38f0
twatanabe <t.watanabe.yami@gmail.com>
2025-10-04 07:18:43 +0900

    外部キーの追加とトランザクションの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+  PaymentMethod.delete_all
+
+  PaymentMethod.create(id: 1, name: 'クレジットカード')
+  PaymentMethod.create(id: 2, name: '銀行振込')
+  PaymentMethod.create(id: 3, name: '代引き')
+  PaymentMethod.create(id: 4, name: 'コンビニ払い')
+  PaymentMethod.create(id: 5, name: '郵便為替')
+end

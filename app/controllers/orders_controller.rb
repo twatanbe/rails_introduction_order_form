7c8733f80f17bcd56741fe5f8b202f02f2743e39
twatanabe <t.watanabe.yami@gmail.com>
2025-10-25 08:37:28 +0900

    メール送信の非同期処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
     return render :new if params[:button] == 'back'
 
     if @order.save
-      OrderMailer.mail_to_user(@order.id).deliver
+      OrderMailerJob.perform_later(@order.id)
       session[:order_id] = @order.id
       return redirect_to complete_orders_url
     end
